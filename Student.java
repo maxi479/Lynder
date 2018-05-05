ceddf2ab5d06852652261381ce52e6666278b1e6
maxwe <maxwe@10.0.0.253>
2018-05-05 11:26:16 -0700

    did stuff

--- Student.java
+++ Student.java
@@ -1,33 +1,69 @@
+import java.util.*;
+
 
 public class Student
+{
+    // total added score from all other students
+    private int popularity;
+
+    // whether or not he has rated every other student in the class
+    private boolean rated;
+
+    // name of student
+    private String name;
+
+    // GPA of student
+    private double gpa;
+
+    // map of all students for rating, pull from array
+    private HashMap<String, Integer> ratings;
+
+
+    /**
+     * default constructor
+     */
+    public Student()
     {
-        // total added score from all other students
-        private int popularity;
-        // whether or not he has rated every other student in the class
-        private boolean rated; 
-        //name of student
-        private String name;
-        //GPA of student
-        private double GPA;
-        
-        /**
-         * default constructor
-         */
-        public Student()
-        {
-            popularity = 0;
-            rated = false;
-            name = "";
-            GPA = 0;
-        }
-        
-        public Student(String fullName, double gradePointAvg)
-        {
-            popularity = 0;
-            rated = false;
-            name = fullName;
-            GPA = gradePointAvg;
-        }
-        
-        
+        popularity = 0;
+        rated = false;
+        name = "";
+        gpa = 0;
     }
+
+
+    public Student( String fullName, double gradePointAvg )
+    {
+        popularity = 0;
+        rated = false;
+        name = fullName;
+        gpa = gradePointAvg;
+    }
+
+
+    /**
+     * creates and updates map from master array
+     */
+    public void updateMap()
+    {
+
+    }
+
+
+    public void rateStudents()
+    {
+        rated = true;
+    }
+
+
+    public String getName()
+    {
+        return name;
+    }
+
+
+    public double getGPA()
+    {
+        return gpa;
+    }
+
+}
